fbe1f4acdc433695a8cee2968335cc4d5f99bb2d
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-03-28 17:02:02 +0530

    - Added changeset for tags module

--- lib/linkly/tags.ex
+++ lib/linkly/tags.ex
@@ -1,5 +1,6 @@
 defmodule Linkly.Tag do
   use Ecto.Schema
+  import Ecto.Changeset
   alias Linkly.{Link, User, LinkTag, Tag}
 
   schema "tags" do
@@ -9,5 +10,12 @@ defmodule Linkly.Tag do
     many_to_many(:links, Link, join_through: LinkTag)
 
     timestamps()
+  end
+
+  def changeset(%Tag{} = tag, attrs) do
+    tag
+    |> cast(attrs, [:title])
+    |> validate_required([:title])
+    |> unique_constraint(:title)
   end
 end
